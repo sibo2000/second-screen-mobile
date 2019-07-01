4fe00c29813fa219176e0e68240c2e74d8a5b4cb
Daniele Bertelle <daniele.bertelle@dazn.com>
2019-07-01 10:30:23 +0100

    refactor(strings) add file for english strings

---
+++ src/constants-EN.js
@@ -0,0 +1,61 @@
+export const PACKAGE = {
+  title: 'Basic monthly package',
+  subtitle1: 'Your free month trial will start today. From',
+  subtitle2: 'you will pay €9.99 (inc tax).'
+}
+
+export const FORM = {
+  first_name: 'First Name',
+  last_name: 'Last Name',
+  email: 'Email',
+  confirm_email: 'Confirm Email',
+  password: 'Password',
+  news_agreement: 'Yes, please send me emails with exclusive content, product updates & notifications of DAZN'
+}
+
+export const ACTIONS = {
+  continue: 'Continue',
+  already_member: 'Already a member?', 
+  sign_in: 'Sign in',
+  start_subscription: 'Start Subscription',
+  start_watching: 'Start watching',
+  get_app: 'Get the app',
+  done: 'Done',
+  cancel_anytime: 'Cancel anytime'
+}
+
+export const FOOTER = {
+  terms: 'Terms of Use',
+  privacy: 'Privacy Policy',
+  imprint: 'Imprint',
+  media: 'Media'
+}
+
+export const PAYMENT = {
+  method: 'Payment method',
+  card: 'Card',
+  bank: 'Bank Account',
+  paypal: 'Paypal',
+  gift_code: 'Gift Code'
+}
+
+export const SUBSCRIPTION = {
+  about: 'About your subscription',
+  package: 'Package',
+  starts: 'Access starts',
+  first_payment: 'First payment date',
+  trial_date: 'Free trial end date',
+  monthly_price: 'Monthly price',
+}
+
+export const SUCCESS = {
+  subtitle1: 'You’re ready to explorer DAZN and watch the best sports from around the world. As a new member, enjoy a one month free trial. On',
+  subtitle2: 'you will start paying [Price] monthly.'
+}
+
+export const HEADING = {
+  create_account: 'Create Account',
+  payment_details: 'Payment Details',
+  you_are_in: 'You’re in',
+  all_set: 'You are all set!'
+}
